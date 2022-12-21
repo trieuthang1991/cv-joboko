1392318f7d09adc54eac2314d4a6e5b151f3673b
thang <trieuthangtin18a1@gmail.com>
2022-12-21 13:55:35 +0700

    Chỉnh mẫu CV

--- 1201/bk/templates/1073/js/tm.js
+++
@@ -1,4 +0,0 @@
-$(document).off('focusout', '.txt-theme').on('focusout', '.txt-theme', function () {
-       var name = $(this).attr("name");
-       $("[name='"+name+"']").text($(this).text())
-});
